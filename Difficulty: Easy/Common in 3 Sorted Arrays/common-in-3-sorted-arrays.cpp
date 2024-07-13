c8fd1c0c348572708a0efb27b6db2fccfc6e6424
Abhishek Singh <92526811+abhi-si@users.noreply.github.com>
2024-07-13 15:35:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find common elements in three arrays.
+    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2,
+                               vector<int> &arr3) {
+        int n1 = arr1.size();
+        int n2 = arr3.size();
+        int n3 = arr3.size();
+        int i = 0, j = 0, k = 0;
+        vector<int>ans;
+        while(i<n1 && j<n2 && k<n3){
+            if(arr1[i] == arr2[j] && arr2[j] ==  arr3[k]){
+                ans.push_back(arr1[i]);
+                int el = arr1[i];
+                while(i<n1 && arr1[i]==el){
+                    i++;
+                }
+                while(i<n1 && arr1[i]==el){
+                    j++;
+                }
+                while(i<n1 && arr1[i]==el){
+                    k++;
+                }
+            }
+            else if(arr1[i] < arr2[j]){
+                i++;
+            }
+            else if(arr2[j] < arr3[k]){
+                j++;
+            }
+            else{
+                k++;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, brr, crr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            brr.push_back(number2);
+        }
+        string input3;
+        getline(cin, input3);
+        stringstream ss3(input3);
+        int number3;
+        while (ss3 >> number3) {
+            crr.push_back(number3);
+        }
+        Solution ob;
+        vector<int> res = ob.commonElements(arr, brr, crr);
+        if (res.size() == 0)
+            cout << -1;
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+    }
+}
+// } Driver Code Ends
